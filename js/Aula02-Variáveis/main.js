c6ba124062be6687f4d624f5b8f0d4806a6ebad0
feh3211 <felipenepomuceno2018@gmail.com>
2023-10-17 21:45:20 -0300

    Js

---
+++ js/Aula02-Variáveis/main.js
@@ -0,0 +1,49 @@
+var nomeDoPrimeiroAluno = 'Eduardo'
+
+console.log ('Mc Cabelinho')
+
+console.log (nomeDoPrimeiroAluno)
+
+var numeroDeAprovados = 5
+
+console.log (numeroDeAprovados)
+
+console.clear()
+
+let nomeAluno =  'Victor'
+
+console.log (nomeAluno)
+
+const nomeSiteWeb = 'facebook'
+
+console.log(nomeSiteWeb)
+
+var numero1 = 10
+
+var numero2 = 4
+
+var soma = numero1 + numero2
+
+console.log (soma)
+
+var texto1 = 'documentos livros etc'
+
+var texto2 = ' filmes séries etc'
+ 
+var concatenar = texto1 + texto2
+
+console.log (concatenar)
+
+console.clear ()
+
+var osDeVerdadeAGenteSabeQuemSão = false
+
+console.log (8 * null)
+
+console.log ('5' - 1)
+
+console.log ('5' + 1)
+
+console.log ('five' - 1)
+
+
