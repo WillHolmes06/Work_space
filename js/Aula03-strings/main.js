1af27111222d495f56c1d7e9ce805a540f3d1364
feh3211 <felipenepomuceno2018@gmail.com>
2023-10-24 21:54:13 -0300

    comitado fi kkk

---
+++ js/Aula03-strings/main.js
@@ -0,0 +1,10 @@
+var texto1 = 'texto aleatorio dos atrasados da volta da pausa'
+
+var texto2 = 'texto aleatorio dos atrasados da volta da pausa'
+
+// console.log(texto1 + '' + 'A vida é um sapinho pulando na lagoa' + '' + texto2)
+
+//template strings
+//console.log(`{texto1} a vida é um sapinho dando pulinhos na lagoa     $`{texto2}`)
+
+var nu
---
+++ js/Aula04-Condicionais/main.js
@@ -0,0 +1,49 @@
+let professoraBateAluno = true
+let professoraXingaAluno = false
+
+if(professoraBateAluno == true && professoraBateAluno == true ) {
+    console.log('professora demitida com sucesso!')
+}
+
+ if (professoraBateAluno == true || professoraXingaAluno == false){
+    console.log('professora demitida com sucesso!')
+}
+
+let numero1 = 10
+let numero2 = 4
+if (numero1>numero2) {
+    console.log('os dois são diferentes!')
+    
+}
+
+let teraComidaTOTVS = true
+if (teraComidaTOTVS == true) {
+    console.log('Não vou nem almoçar, Alexsandro o Brabo')
+}
+
+let numero = 11
+
+if (numero % 2 == 0) {
+    console.log('Esse número é par')
+}else{
+    console.log('Esse número é impar')
+}
+
+let caminhoDasIndias = 99
+
+let idade = 10
+
+if (caminhoDasIndias == 9) {
+    console.log ('Você está ficando velho')
+    if (idade = 10) {
+        console.log('cara, você ta com um pé na cova')
+    }else {
+        console.log('você está jovem')
+    }
+
+}
+
+
+
+
+
